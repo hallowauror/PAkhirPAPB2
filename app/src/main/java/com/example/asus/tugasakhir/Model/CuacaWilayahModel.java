f35e33e8fe8df3feff600ef5f07484a92e13c536
Ega Arya <ega0110@gmail.com>
2019-05-02 14:10:34 +0700

    Tugas Akhir

---
+++ app/src/main/java/com/example/asus/tugasakhir/Model/CuacaWilayahModel.java
@@ -0,0 +1,13 @@
+package com.example.asus.tugasakhir.Model;
+
+public class CuacaWilayahModel {
+
+        public PlaceModel place;
+        public CuacaModel model;
+
+        public CuacaWilayahModel(PlaceModel place, CuacaModel model) {
+            this.place=place;
+            this.model=model;
+        }
+}
+
